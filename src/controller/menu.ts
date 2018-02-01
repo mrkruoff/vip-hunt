0bfbf9927013f19ff1f73f86c86be18919427fb0
Howard Chen <H_Chen@alumni.baylor.edu>
2018-01-31 17:07:30 -0800

    Added basic menu for game

--- src/controller/menu.ts
+++ src/controller/menu.ts
@@ -12,25 +12,71 @@ const displayWelcome = function() {
     const color = 'white';
     const alignment = 'center';
     const welcomeText = new TextSprite('Welcome to Potayto-Potahto!', '32px Verdana', color, alignment);
-    const welcomeObject = new SceneObject(welcomeText);
-    wade.addSceneObject(welcomeObject);
-    welcomeObject.setPosition(0, -100);
+    this.welcomeObject = new SceneObject(welcomeText);
+    wade.addSceneObject(this.welcomeObject);
+    this.welcomeObject.setPosition(0, -100);
 
     const newGameText = new TextSprite('New Game', '20px Verdana', color, alignment);
-    const newGameObject = new SceneObject(newGameText);
-    wade.addSceneObject(newGameObject);
-    newGameObject.setPosition(0, 0);
+    this.newGameObject = new SceneObject(newGameText);
+    wade.addSceneObject(this.newGameObject);
+    setupNewGame.call(this);
 
-    const saveGameText = new TextSprite('Load Game', '20px Verdana', color, alignment);
-    const saveGameObject = new SceneObject(saveGameText);
-    wade.addSceneObject(saveGameObject);
-    saveGameObject.setPosition(0, 50);
+    const loadGameText = new TextSprite('Load Game', '20px Verdana', color, alignment);
+    this.loadGameObject = new SceneObject(loadGameText);
+    wade.addSceneObject(this.loadGameObject);
+    setupSaveGame.call(this);
 
     const settingsText = new TextSprite('Settings', '20px Verdana', color, alignment);
-    const settingsObject = new SceneObject(settingsText);
-    wade.addSceneObject(settingsObject);
-    settingsObject.setPosition(0, 100);
+    this.settingsObject = new SceneObject(settingsText);
+    wade.addSceneObject(this.settingsObject);
+    setupSettings.call(this);
 
 };
+
+function setupSettings() {
+    this.settingsObject.setPosition(0, 100);
+    setMouseInOut(this.settingsObject);
+
+    // this.settingsObject.onClick = settings.call(this);
+    // wade.addEventListener(this.loadGameObject, 'onClick');
+
+}
+
+function setupSaveGame() {
+    this.loadGameObject.setPosition(0, 50);
+
+    setMouseInOut(this.loadGameObject);
+
+    // this.loadGameObject.onClick = loadGame.call(this);
+    // wade.addEventListener(this.loadGameObject, 'onClick');
+
+}
+
+function setupNewGame() {
+    this.newGameObject.setPosition(0, 0);
+
+    setMouseInOut(this.newGameObject);
+
+    // this.newGameObject.onClick = startGame.call(this);
+    // wade.addEventListener(this.newGameObject, 'onClick');
+}
+
+// Helper function that returns a function that will change a textObject's
+// color
+function changeColor(textObject: any, color: string) {
+    return function(event: any) {
+        textObject.getSprite(0).setColor(color);
+    };
+}
+
+// Helper function that adds color changes to a textObject on the
+// mouseIn and MouseOut events
+function setMouseInOut(textObject: any) {
+    textObject.onMouseIn = changeColor(textObject, 'red');
+    wade.addEventListener(textObject, 'onMouseIn');
+
+    textObject.onMouseOut = changeColor(textObject, 'white');
+    wade.addEventListener(textObject, 'onMouseOut');
+}
 
 export { displayWelcome };
