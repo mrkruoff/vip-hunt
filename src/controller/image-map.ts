e08bec5a79adf9f3be6954c594d70e1c83a0549e
Howard Chen <H_Chen@alumni.baylor.edu>
2018-02-06 17:22:01 -0800

    Added a Mouse enum for Mouse clicks

--- src/controller/image-map.ts
+++ src/controller/image-map.ts
@@ -1,8 +1,9 @@
 
-let ImageMap = {
-    scroll: '../public/sprites/hud/scroll.png',
-    buildingIcon: '../public/sprites/hud/building.png',
-    barracks1: '../public/sprites/buildings/barracks_1.png',
+// The root directory is the html folder. But because the Javascript is in the  /js folder, we must prepend ../js/ to all the image URLs.
+const ImageMap = {
+    scroll: '../js/../public/sprites/hud/scroll.png',
+    buildingIcon: '../js/../public/sprites/hud/building.png',
+    barracks1: '../js/../public/sprites/buildings/barracks_1.png',
 
 };
 
---
+++ src/controller/mouse.ts
@@ -0,0 +1,8 @@
+
+const Mouse = {
+    left: 0,
+    middle: 1,
+    right: 2,
+};
+
+export default Mouse;
