5f6c4974ce3723c5a0ddd04dd5d8ea77ea3b234a
Howard Chen <H_Chen@alumni.baylor.edu>
2018-02-14 08:02:17 -0800

    Added loading of cost JSON files

--- src/controller/json-map.ts
+++ src/controller/json-map.ts
@@ -18,11 +18,21 @@ const JsonMap = {
     spear_calvary_data: '../js/../json/units/spear_calvary_unit.json',
     swordsman_data: '../js/../json/units/swordsman_unit.json',
     vip_data: '../js/../json/units/vip_unit.json',
-
     barracks_data: '../js/../json/buildings/barracks_building.json',
     stables_data: '../js/../json/buildings/stable_building.json',
     townhall_data: '../js/../json/buildings/townhall_building.json',
     tower_data: '../js/../json/buildings/watchtower_building.json',
+
+    archer_calvary_cost: '../js/../json/costs/archer_calvary_cost.json',
+    archer_cost: '../js/../json/costs/archer_cost.json',
+    drummer_boy_cost: '../js/../json/costs/drummer_boy_cost.json',
+    gatherer_cost: '../js/../json/costs/gatherer_cost.json',
+    spear_calvary_cost: '../js/../json/costs/spear_calvary_cost.json',
+    swordsman_cost: '../js/../json/costs/swordsman_cost.json',
+    barracks_cost: '../js/../json/costs/barracks_cost.json',
+    stables_cost: '../js/../json/costs/stable_cost.json',
+    townhall_cost: '../js/../json/costs/townhall_cost.json',
+    tower_cost: '../js/../json/costs/tower_cost.json',
 
 };
 
