9c91ba81132826ad45ec98d35ee867d37dccb72d
Howard Chen <H_Chen@alumni.baylor.edu>
2018-02-18 19:25:49 -0800

    Initial implementation of AI unit construction function

--- src/controller/ai-gameplay.ts
+++ src/controller/ai-gameplay.ts
@@ -18,6 +18,14 @@ import TownHall from '../model/buildings/townhall_buildings';
 import JsonMap from './json-map';
 import Tile from '../model/map/tile';
 import AiGameState from '../model/state/ai-game-state';
+import Archer from '../model/units/archer_unit';
+import ArcherCalvary from '../model/units/archerCalvary_unit';
+import DrummerBoy from '../model/units/drummerBoy_unit';
+import Gatherer from '../model/units/gatherer_unit';
+import SpearCalvary from '../model/units/spearCalvary_unit';
+import Swordsman from '../model/units/swordsman_unit';
+import Unit from '../model/units/units';
+import VIP from '../model/units/VIP_unit';
 
 declare var wade: any;
 declare var TextSprite: any;
@@ -97,7 +105,7 @@ var AiGamePlay = {
         // and clear its old tile
         GamePlay.updateResourceMapLocation(sceneBuilding);
     },
-    unitMove: (id: number, x:number, y: number) => {
+    unitMove: (id: number, x:number, z: number) => {
         let state = wade.getSceneObject('global').state;
         let ai = state.getAi();
         let map = state.getMap();
@@ -114,14 +122,83 @@ var AiGamePlay = {
             unitSceneObject.getBehavior('IsoCharacter').goToObject(objects[0]);
         } else {
             // If nothing is there, move ther directly. 
-            unitSceneObject.getBehavior('IsoCharacter').setDestination({x: x, z: y});
+            unitSceneObject.getBehavior('IsoCharacter').setDestination({x: x, z: z});
         }
         //Keep track of the movements in the internal game state.
         GamePlay.move(unitSceneObject);
     
     },
-    constructUnit: (className: string, x: number, y: number) => {
-    
+    constructUnit: (className: string, x: number, z: number) => {
+        let state = wade.getSceneObject('global').state; 
+        let ai: AiGameState = state.getAi();
+        let map: Array<Tile[]> = state.getMap();
+
+        let u: Unit;
+
+        //Build the correct Unit model
+        if(_.isEqual(className, "Archer")) {
+            u = Archer.fromObject(wade.getJson(JsonMap.archer_data)); 
+        } else if (_.isEqual(className, "ArcherCalvary")) {
+            u = ArcherCalvary.fromObject(wade.getJson(JsonMap.archer_calvary_data)); 
+        } else if (_.isEqual(className, "SpearCalvary")) {
+            u = SpearCalvary.fromObject(wade.getJson(JsonMap.spear_calvary_data)); 
+        } else if (_.isEqual(className, "Swordsman")) {
+            u = Swordsman.fromObject(wade.getJson(JsonMap.swordsman_data)); 
+        } else if (_.isEqual(className, "Gatherer")) {
+            u = Gatherer.fromObject(wade.getJson(JsonMap.gatherer_data)); 
+        } else if (_.isEqual(className, "DrummerBoy")) {
+            u = DrummerBoy.fromObject(wade.getJson(JsonMap.drummer_boy_data)); 
+        } else if (_.isEqual(className, "VIP")) {
+            u = VIP.fromObject(wade.getJson(JsonMap.vip_data)); 
+        }
+        else {
+            console.error("Incorrect class name in AiGamePlay.constructUnit!"); 
+        }
+        //Give it an id.
+        u.id = Id.getId();
+        //Put it on the map and in the Ai's state.
+        map[z][x].unitId = u.getId();
+        ai.getUnits().push(u);
+
+        //Use the Unit model to build an associated sprite
+        // This may FAIL due to a BUG: collision at 0, 0
+        let sceneUnit = UnitBuilding.constructUnitFromModel(u);
+        sceneUnit.oldX = x;
+        sceneUnit.oldZ = z;
+
+        //Link the data and the representation
+        sceneUnit.data = u;
+        u.rep = sceneUnit;
+        
+        //Place the unit where indicated. IF THIS FAILS IT WAS BECAUSE OF A COLLISION.
+        let wasMoved = false;
+        let sideLength = map[0].length;
+        while(!wasMoved) {  
+            //Randomly shift x and z until it is placed properly.
+            let changeX = Math.floor(Math.random() * 2);
+            let shouldIncrease = Math.floor(Math.random() * 3); //more likely to increase
+            let step = Math.floor(Math.floor(Math.random() * 3)) + 1;
+            if(!shouldIncrease) {
+                step = -1 * step; 
+            }
+            if(changeX) {
+                x = (x + step) % sideLength; 
+                if(x < 0) { 
+                    x += sideLength; 
+                }
+            } else {
+                z = (z + step) % sideLength; 
+                if(z < 0) {
+                    z += sideLength; 
+                }
+            }
+
+            wasMoved = wade.iso.moveObjectToTile(sceneUnit, x, z);
+        }
+        
+        //Once the sprite is properly moved, update its map location in the state.
+        // and clear its old tile
+        GamePlay.updateUnitMapLocation(sceneUnit);
     }
 
 
