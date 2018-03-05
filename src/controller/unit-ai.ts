c05ba75d17e43551d5f503ef6e5238c92bd17e9a
Mark Ruoff <ruoffm@oregonstate.edu>
2018-03-04 20:42:33 -0800

    Initial outline, todo range search

---
+++ src/controller/unit-ai.ts
@@ -0,0 +1,37 @@
+import GlobalGameState from "./global-game-state"
+import JsonMap from './json-map';
+
+
+declare var wade: any;
+
+async function delay(milliseconds: number) {
+    return new Promise<void>((resolve) => {
+        wade.setTimeout(resolve, milliseconds);
+ });
+} 
+
+
+const unitDec = {
+	 loop: async (globalState, unit) =>{ 
+		while(true){
+			await delay(1500);
+			let aistate=globalState.getAi();
+			let playerstate=globalState.getPlayer();
+			//if unit is a player unit
+			if(!(unit in aistate.units)){
+				
+			}
+			//if unit is an ai unit
+			else{
+				
+			}
+		}
+		 
+	 },
+	
+	
+}; 
+
+
+
+export default unitDec;
